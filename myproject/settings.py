93cedc618aca94cd72638610c648b2923a4d7977
Olabode Ezekiel <ayoeze191@gmail.com>
2025-07-14 13:10:20 +0100

    Add nixpacks build config

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-974ub%pu^flm^j@%k1cz!-(xi+9i5p)%i=0h1db^i_s-%0%a8l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://web-production-f0c71.up.railway.app/']
 
 
 # Application definition
